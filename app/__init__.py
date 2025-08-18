30bd66b47151d087902d08aa1e242142eb99de58
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-08-18 11:37:12 -0300

    Initial commit
    Creating the initial structure of project

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import *
+
+
+#inicializar extensoes do db, migrate e sockets aqui
+
+def createApp():
+    #inicialização de extenções
+    app = Flask(__name__)
+
+    #reistro de rotas via blue print
+    #retornando a intancia
+    return app
+
+   
+
