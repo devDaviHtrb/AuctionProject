8bbc8329bbb477472fcfcdbf3eb5303c5c215ef3
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-09-10 23:41:11 -0300

    <fix> hardcode error

--- myapp/setup/registerRoutes.py
+++ myapp/setup/registerRoutes.py
@@ -9,11 +9,16 @@ import os
 def register_routes(app, folder="myapp/routes", package="myapp.routes"):
     folder = os.path.abspath(folder)
     for item in os.listdir(folder):
+
         if  item!="__pycache__":
+            #verifying if is a module
             if item.endswith(".py") == False:
-                register_routes(app, f"myapp/routes/{item}", f"{package}.{item}")
+                #opening the folder
+                register_routes(app, f"{folder}/{item}", f"{package}.{item}")
             else:
+
                 module = import_module(f"{package}.{item[:-3]}")
+
                 for ignore1, obj in getmembers(module):
                     if isinstance(obj, Blueprint):
                         app.register_blueprint(obj)
