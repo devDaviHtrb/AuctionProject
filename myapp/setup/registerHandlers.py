dc63f9afee5c79ca43cf1244fd009a6e2b5a6976
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-09-30 23:32:57 -0300

    <add> commentary

--- myapp/setup/registerHandlers.py
+++ myapp/setup/registerHandlers.py
@@ -12,12 +12,11 @@ def register_handlers(app: Flask) -> None:
             code = int(module_name[:3])
             error_pages[code] = f"{code}.html"
 
-   
     def handle_error(err: object) -> Tuple[str, int]:
-        code = getattr(err, "code", 500)  
-        template = error_pages.get(code, "500.html")
+        code = getattr(err, "code")
+        # Closure: this function is called when an error is aborted, so it can remember and access the error_pages dictionary because it is in the scope of the register_handlers function.
+        template = error_pages.get(code) 
         return render_template(template), code
-
 
     for code in error_pages:
         app.register_error_handler(code, handle_error)
