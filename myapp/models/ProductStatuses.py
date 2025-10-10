6749e23a13e2a1600fb2a6840ad6e01f982da1d9
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-10-10 19:46:17 -0300

    <fix> column name

--- myapp/models/ProductStatuses.py
+++ myapp/models/ProductStatuses.py
@@ -2,4 +2,4 @@ from myapp.setup.InitSqlAlchemy import db
 from sqlalchemy import ForeignKey
 class product_statuses(db.Model):
     product_status_id = db.Column(db.Integer, primary_key=True)
-    payment_status = db.Column(db.String(20), nullable=False)
+    product_status = db.Column(db.String(20), nullable=False)
