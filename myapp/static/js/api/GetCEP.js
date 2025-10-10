55838bdd9ff1339381ff095c2d9c49a363cc1152
DevLia13 <giulia.oliveira@aluno.ifsp.edu.br>
2025-10-09 22:23:33 -0300

    Add files via upload

---
+++ myapp/static/js/api/GetCEP.js
@@ -0,0 +1,21 @@
+// Busca CEP
+    cepInput.addEventListener('blur', async () => {
+      const cep = cepInput.value.replace(/\D/g, '');
+      if(cep.length === 8){
+        try {
+          const res = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+          const data = await res.json();
+          if(!data.erro){
+            ruaInput.value = data.logradouro;
+            bairroInput.value = data.bairro;
+            cidadeInput.value = data.localidade;
+            estadoInput.value = data.uf;
+          } else {
+            alert("CEP não encontrado!");
+          }
+        } catch(err){
+          alert("Erro ao buscar CEP");
+          console.error(err);
+        }
+      }
+    });
