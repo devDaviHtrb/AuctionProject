6fc7b8f0161b220c2f677b0c9e86849da135c447
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-10-09 21:37:27 -0300

    <fix> room

--- myapp/sockets/Room.py
+++ myapp/sockets/Room.py
@@ -2,6 +2,7 @@ from flask_socketio import emit, join_room, rooms
 from typing import Dict, Any
 from myapp.setup.InitSocket import socket_io
 from flask import request
+from flask_login import current_user
 anonymous_users_number = 0
 @socket_io.on("join_room")
 def handle_join(data: Dict[str, Any]) -> None:
@@ -10,10 +11,8 @@ def handle_join(data: Dict[str, Any]) -> None:
     response = {
         "type": "entry",
         "room_id": room_id,
-        "user_id": data.get("user_id"),
-        "username": data.get("username") if not request.cookies.get("anonymous") else f"AnonymousUser{anonymous_users_number}",
-        "product_id": data.get("product_id"),
-        "product_name": data.get("product_name")
+        "user_id": current_user.user_id,
+        "username": current_user.username if not request.cookies.get("anonymous") else f"AnonymousUser{anonymous_users_number}",
     }
     anonymous_users_number+=1
     emit("server_content", {"response": response}, to=room_id)
@@ -24,8 +23,6 @@ def handle_content(data: Dict[str, Any]) -> None:
     auction_rooms = [r for r in rooms() if r != request.sid]
     room_id = auction_rooms[0]
 
-    user_id = data.get("user_id"),
-    username = data.get("username")
     value = data.get("value")
     product_id = data.get("product_id")
     product_name = data.get("product_name")
@@ -33,8 +30,8 @@ def handle_content(data: Dict[str, Any]) -> None:
     response = {
         "type": "bid",
         "room_id": room_id,
-        "user_id": user_id,
-        "username": username,
+        "user_id": current_user.user_id,
+        "username": current_user.username if not request.cookies.get("anonymous") else f"AnonymousUser{anonymous_users_number}",
         "value": value,
         "product_id": product_id,
         "product_name": product_name
