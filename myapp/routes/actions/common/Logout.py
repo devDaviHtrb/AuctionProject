0b367da905c55e7ae78a0437aeaa8ad3a028c99f
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-10-13 23:19:26 -0300

    <fix> logout

--- myapp/routes/actions/common/Logout.py
+++ myapp/routes/actions/common/Logout.py
@@ -1,10 +1,12 @@
 
-from flask import Blueprint, redirect, url_for, Response
-from flask_login import logout_user
+from flask import Blueprint, redirect, session, url_for, Response
+
 logout = Blueprint("logout", __name__)
 
 
 @logout.route("/logout", methods=["GET", "POST"])
 def Logout() -> Response:
-    logout_user()
+    session["id"] = None
+    session["username"] = None
+    session["admin"] = None
     return redirect(url_for("home.Home"))
