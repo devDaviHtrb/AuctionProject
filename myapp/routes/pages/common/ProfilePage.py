36bff894ae530bd73786290c207bdd27484d8fb4
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-10-28 00:12:38 -0300

    <add> profile

--- myapp/routes/pages/common/ProfilePage.py
+++ myapp/routes/pages/common/ProfilePage.py
@@ -1,8 +1,13 @@
-from flask import render_template, Blueprint, Response
+from flask import render_template, Blueprint, Response, session
+from myapp.models.Users import users
+from myapp.setup.InitSqlAlchemy import db
 
 profile = Blueprint("profilePage", __name__)
 
 @profile.route("/profile")
 
 def ProfilePage() -> Response:
-    return render_template("Profile.html")
+    user = users.query.filter_by(user_id = session["user_id"]).first()
+    print(user)
+
+    return render_template("Profile.html", name=session["username"], email=user.email, photo_url = user.photo )
