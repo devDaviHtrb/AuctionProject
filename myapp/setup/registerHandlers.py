01d9bdc5203544f3ef53e64ac61c0a347d932f33
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-11-02 13:18:35 -0300

    <add> linking header component to the sign in/ sign Up button

--- myapp/setup/registerHandlers.py
+++ myapp/setup/registerHandlers.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 from typing import Tuple
 
 def register_handlers(app: Flask) -> None:
-    error_codes = [403, 404, 401]
+    error_codes = [403, 404]
     error_pages = {}
     
     for code in error_codes:
