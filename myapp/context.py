ae49dfe8a3a4a0136cbb7def5d80d59edc9dae41
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-11-02 16:35:31 -0300

    <fix> logout

--- myapp/context.py
+++ myapp/context.py
@@ -11,6 +11,7 @@ def init_context(app: Flask) -> None:
         user_name = session.get("username")
         user_wallet = session.get("user_wallet")
         user_photo = session.get("user_photo", "#")
+        
 
         user_context = {
             "id": user_id,
--- myapp/routes/actions/common/Logout.py
+++ myapp/routes/actions/common/Logout.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, redirect, session, url_for, Response
+from flask import Blueprint, redirect, session, url_for, Response, request, make_response
 from myapp.utils.LinksUrl import HOME_PAGE, home
 from typing import Tuple
 
@@ -8,7 +8,9 @@ logout_bp = Blueprint("logout", __name__)
 
 @logout_bp.route("/logout", methods=["GET", "POST"])
 def logout() -> Tuple[Response, int]:
+    response = make_response(redirect(url_for(HOME_PAGE)))
+    response.delete_cookie("user_id")
     for key in session.keys():
         session[key] = None
         print(session[key])
-    return redirect(url_for(HOME_PAGE)), 200
+    return response, 200
