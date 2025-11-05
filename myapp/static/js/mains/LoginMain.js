2ac1b4bdfc803de8922cd35a5e6e4954c743e932
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-11-04 23:56:21 -0300

    <fix> login

--- myapp/static/js/mains/LoginMain.js
+++ myapp/static/js/mains/LoginMain.js
@@ -8,15 +8,10 @@ const sign_up = document.getElementById("sign-up");
 buttonSubmit.addEventListener("click", async (e) => {
   message.innerHTML = "";
 
-  const username = document.getElementById("username").value.trim();
-  const password = document.getElementById("password").value.trim();
-  const notRobot = document.getElementById("notRobot").checked;
+  const username = document.getElementById("login-email").value.trim();
+  const password = document.getElementById("login-password").value.trim();
 
-  if (login_validation(username, password, notRobot, message) == true) {
+  if (login_validation(username, password, message) == true) {
     await login({ username: username, password: password });
   }
 });
-
-sign_up.addEventListener("click", async (e) => {
-  window.location.href = sign_up.dataset.url;
-})
