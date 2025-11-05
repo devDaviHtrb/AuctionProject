413cecdc9bde224a9d0f9260f6117d92590bd3f9
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-11-05 01:54:59 -0300

    <del> bios error

--- myapp/routes/pages/common/AuctionsPage.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, Blueprint
-
-auctions = Blueprint("auctionsPage", __name__)
-
-@auctions.route("/auctions")
-def AuctionsPage():
-    return render_template("Auctions.html")
