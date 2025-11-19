600bf16f9b2274d10c343492df005a1f230dc1f3
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-11-18 23:58:18 -0300

    <fix> route name

--- myapp/routes/pages/common/UserBidsPage.py
+++ myapp/routes/pages/common/UserBidsPage.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 user_bids_bp = Blueprint("userBids", __name__)
 
-@user_bids_bp.route("/userBids")
+@user_bids_bp.route("/userBidsPage")
 def user_bids_page():
         return render_template("UserBidsPage.html")
