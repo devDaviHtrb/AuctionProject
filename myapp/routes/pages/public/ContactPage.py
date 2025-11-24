0f80d423494fb0b8d3791b46ef0d11fde048a319
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-11-24 13:53:30 -0300

    <fix> syntax error

--- myapp/routes/pages/public/ContactPage.py
+++ myapp/routes/pages/public/ContactPage.py
@@ -2,6 +2,6 @@ from flask import render_template, Blueprint, Response
 
 contact_page_bp = Blueprint("contactPage", __name__)
 
-@contact_page_bp.routes("/contactPage")
+@contact_page_bp.route("/contactPage")
 def Contact():
     return render_template("Contact.html")
