7c7a5117a4c0c695585dcbca7da48ab7247a8632
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-11-25 23:44:56 -0300

    <test> fixing the footer

--- myapp/routes/actions/admin/UpImage.py
+++ myapp/routes/actions/admin/UpImage.py
@@ -9,8 +9,9 @@ HTML = """
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Upload de Imagem</title>
-</head>
-<body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head> <body class="d-flex flex-column min-vh-100">
   <h1>Enviar uma Imagem</h1>
   <form action="/upImage" method="POST" enctype="multipart/form-data">
     <input type="file" name="image" accept="image/*" required>
