548d6ff5a371422184a0b5e5792aff719938ffe6
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-11-26 00:15:37 -0300

    <fix> css

--- myapp/routes/pages/public/Profile.py
+++ myapp/routes/pages/public/Profile.py
@@ -1,4 +1,6 @@
-from flask import render_template, Blueprint, Response
+from flask import render_template, Blueprint, Response, session
+
+from myapp.repositories.GetBids import get_interesting_user_bids
 
 
 profile = Blueprint("profile", __name__)
@@ -7,4 +9,5 @@ profile = Blueprint("profile", __name__)
 @profile.route("/profile")
 
 def Profile() -> Response:
-    return render_template("Profile.html")
+    user_bids = get_interesting_user_bids(user_id=session.get("user_id"))
+    return render_template("Profile.html", bids = [bid for bid in user_bids if user_bids.index(bid)<=2])
