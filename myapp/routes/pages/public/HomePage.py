5e75aebf3bcb6e3beddf0e165df08b8f04105b9a
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-11-26 23:42:15 -0300

    <fix> config bug

--- myapp/routes/pages/public/HomePage.py
+++ myapp/routes/pages/public/HomePage.py
@@ -1,10 +1,13 @@
 from flask import render_template, Blueprint, Response
 
+from myapp.repositories.ProductRepository import get_and_images_and_status_diffents_valids_randomly
+
 home = Blueprint("homePage", __name__)
 
 @home.route("/")
 def HomePage() -> Response:
     return render_template(
+       
         "Index.html",
         # fix it
         top_products = [
